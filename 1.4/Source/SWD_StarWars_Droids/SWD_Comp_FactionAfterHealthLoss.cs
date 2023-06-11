28636ada8d3f93bc52ce5ae2df4819c80544d8c0
FantasyFanGitHub <60392272+FantasyFanGitHub@users.noreply.github.com>
2023-06-11 21:18:37 +0200

    Add files via upload

---
+++ 1.4/Source/SWD_StarWars_Droids/SWD_Comp_FactionAfterHealthLoss.cs
@@ -0,0 +1,46 @@
+﻿using Verse;
+using RimWorld;
+using System.Collections.Generic;
+using Verse.AI.Group;
+
+namespace SWD_StarWars_Droids
+{
+    public class SWD_Comp_FactionAfterHealthLoss : ThingComp
+    {
+        public int tickCounter = 0;
+        bool SetFactionOnce = true;
+
+        public SWD_CompProperties_FactionAfterHealthLoss Props
+        {
+            get
+            {
+                return (SWD_CompProperties_FactionAfterHealthLoss)this.props;
+            }
+        }
+
+        public override void CompTick()
+        {
+            base.CompTick();
+            tickCounter++;
+
+            if (tickCounter > Props.tickInterval && SetFactionOnce)
+            {
+                Pawn pawn = this.parent as Pawn;
+                if (pawn != null && pawn.Map != null && !pawn.Dead && !pawn.Downed)
+                {
+                    if (pawn.health.summaryHealth.SummaryHealthPercent < ((float)(Props.healthPercent) / 100))
+                    {
+                        var faction = Find.FactionManager.RandomEnemyFaction(allowNonHumanlike: false);
+                        pawn.SetFaction(faction);
+
+                        var map = pawn.Map;
+                        LordMaker.MakeNewLord(faction, new LordJob_AssaultColony(faction, true, false, false, true, true, false, true), map, new List<Pawn> { pawn });
+
+                        SetFactionOnce = false;
+                    }
+                }
+                tickCounter = 0;
+            }
+        }
+    }
+}
---
+++ 1.4/Source/SWD_StarWars_Droids/SWD_DefOf.cs
@@ -0,0 +1,11 @@
+﻿using RimWorld;
+using Verse;
+
+namespace SWD_StarWars_Droids
+{
+    [DefOf]
+    public static class SWD_DefOf
+    {
+        public static PawnKindDef SWD_Viper_Probe_Droid_Enemy;
+    }
+}
